4ac7ea1b770e688b2d6745662d3e89d3a0e8adde
rahulkrajput <unreadtask@gmail.com>
2025-10-26 21:34:54 +0530

    Update 02-variables.tf

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -18,7 +18,7 @@ variable "resource_group_name" {
 variable "environment" {
   type = string  
   description = "This variable defines the Environment"  
-  default = "operation"
+  default = "stage"
 }
 
 # Azure AKS SSH Public Key
